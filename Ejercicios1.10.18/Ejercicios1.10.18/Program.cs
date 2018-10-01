1d4d9a168f4f98d6037d9887f9b17fb530731496
Jon Carrasco <jcarrascobar@gmail.com>
2018-10-01 18:35:48 +0200

    commit 1.10.18

---
+++ Ejercicios1.10.18/Ejercicios1.10.18/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios1._10._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //////programa que dice hola Jon
+            //Console.WriteLine("HolaJon");
+            ////program que suma dos numeros
+            //int num1 = 5; int num2 = 6;
+            //Console.WriteLine(num1 + num2);
+            //// program que calcule la division de dos numeros y las muestre en pantalla
+            //Console.WriteLine(num1 / num2);
+
+
+            ////escribe un program que muestre las siguientes operaciones
+            //double num3 = -5 + (8 * 6);
+            //Console.WriteLine(num3);
+            //double num4 = (55 + 9) % 9;
+            //Console.WriteLine(num4);
+            //double num5 = 20 + (-3 * (5 / 8));
+            //Console.WriteLine(num5);
+            //double num6 = (5 + 15) / 3 * (2 - 8) % 3;
+            //Console.WriteLine(num6);
+
+
+            ////// introducir un numero y luego y otro
+            //Console.WriteLine("introducir un numero");
+            //int num10, num11;
+            ////es requerido hacer la conversión por ello se pone la función convert.
+            //num10 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("introducir otro numero");
+            //num11 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("La multiplicación entre" + num10 + "y" + num11 + "es" + (num10 * num11));
+
+            //// introducir numeros y luego resultados de sumar, multiplicar,....
+            //Console.WriteLine("introducir un numero");
+            //int num12, num13;
+            ////es requerido hacer la conversión por ello se pone la función convert.
+            //num12 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("introducir otro numero");
+            //num13 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("La multiplicación entre" + num12 + "y" + num13 + "es" + (num12 * num13));
+            //Console.WriteLine("la suma entre" + num12 + "y" + num13 + "es" + (num12 + num13));
+            //Console.WriteLine("la resta entre" + num12 + "y" + num13 + "es" + (num12 - num13));
+            //Console.WriteLine("la división entre" + num12 + "y" + num13 + "es" + (num12 / num13));
+            //Console.WriteLine("el resto entre" + num12 + "y" + num13 + "es" + (num12 % num13));
+
+            //// mostrar la tabla de multiplicar
+            //Console.WriteLine("introducir un numero");
+            //int num14;
+            ////es requerido hacer la conversión por ello se pone la función convert.
+            //num14 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(num14 * 1);
+            //Console.WriteLine(num14 * 2);
+            //Console.WriteLine(num14 * 3);
+            //Console.WriteLine(num14 * 4);
+            //Console.WriteLine(num14 * 5);
+            //Console.WriteLine(num14 * 6);
+            //Console.WriteLine(num14 * 7);
+            //Console.WriteLine(num14 * 8);
+            //Console.WriteLine(num14 * 9);
+            //Console.WriteLine(num14 * 10);
+
+            //// calculos con decimales
+            //double num15 = (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5);
+            //Console.WriteLine(num15);
+
+
+            //// calculos con decimales II
+            //double num16 = 4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11));
+            //Console.WriteLine(num16);
+
+            //// calculo del perimetro y area de un circulo
+            //Console.WriteLine("introducir el radio");
+            //double num17;
+            //num17 = Convert.ToDouble(Console.ReadLine());
+            //const double PI = 3.141593;
+            //Console.WriteLine(PI * (num17 * num17));
+            //Console.WriteLine(PI * (2 * num17));
+
+            //// petición de numeros y calcular la media
+            //Console.WriteLine("introducir un numero");
+            //double num18, num19, num20;
+            //num18 = Convert.ToDouble(Console.ReadLine());
+            //num19 = Convert.ToDouble(Console.ReadLine());
+            //num20 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine(num18 / 2);
+            //Console.WriteLine(num19 / 2);
+            //Console.WriteLine(num20 / 2);
+
+            ////calcular el area y perimetro de un cuadrado
+            //Console.WriteLine("introducir altura");
+            //int num21, num22;
+            //num21 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("introducir anchura");
+            //num22 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(num21 * num22);
+            //Console.WriteLine(2 * (num21 + num22));
+
+            //// pedir variables e intercambiar sus valores
+            //Console.WriteLine("variable a");
+            //int num23, num24, num25;
+            //num23 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("variable b");
+            //num24 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(num23 = num24);
+            //num25 = num23;
+            //num23 = num24;
+            //num24 = num25;
+            //Console.WriteLine("la variable a es" + num23);
+            //Console.WriteLine("la variable b es" + num24);
+
+            ////introducir tres numeros y sumarlos
+            //Console.WriteLine("introducir numero");
+            //int num26;
+            //num26 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine((num26 / 100) + ((num26 % 100) / 10) + (num26 % 10));
+
+            //// insertar n y sumar n+nn+nnn
+            //Console.WriteLine("introducir valor n");
+            //int n;
+            //n = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(n + (n + (n * 10)) + (n + (n * 10) + (n * 100)));
+
+            ////pida un numero y pase de sting a int
+            //Console.WriteLine("introducir un valor");
+            //int valor;
+            //valor = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine();
+
+            ////calcular el resto sin usar %
+            //double num34 = 22, num35 = 5;
+            //Console.WriteLine(num34 - (num35 * (num34 / num35);
+
+
+            ////pedir usuario en pulgadas y pasar a metros.
+            //Console.WriteLine("introducir pulgadas");
+            //double num36;
+            //num36 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine(num36 * 0.0254);
+
+            ////pide minutos y da en horas y días
+            //Console.WriteLine("introducir segundos");
+            //double segundos;
+            //segundos = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine((segundos / 60) / 60);
+            //Console.WriteLine(((segundos/60)/60)/24);
+
+            ////pide distncia y tiempo y genera velocidad
+            //Console.WriteLine("introducir distancia");
+            //double metros;
+            //metros = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("introducir tiempo");
+            //double horas, minutos, segundos;
+            //horas = Convert.ToDouble(Console.ReadLine());
+            //minutos = Convert.ToDouble(Console.ReadLine());
+            //segundos = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("velocidad en m/s =  " + metros / segundos);
+            //Console.WriteLine("Velocidad en Km/h =  " + (metros / 1000) / (segundos / 3600));
+
+            //generar numero aleatorio de 1 a 100
+
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
